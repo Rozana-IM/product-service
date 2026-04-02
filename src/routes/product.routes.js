b9d8efdd64270df2c7f721086f5c9ba0b300312c
Rozana-IM <rozanaim09@gmail.com>
2026-04-03 00:45:18 +0530

    Update product.routes.js

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const productController = require("../controllers/product.controller");
 
+const { verifyToken, isAdmin } = require("../middleware/auth");
+
+router.post("/", verifyToken, isAdmin, createProduct);
+router.get("/", getAllProducts);
+router.get("/health", (req,res)=>res.send("OK"));
+
 router.get("/", productController.getProducts);
 router.get("/category/:category", productController.getCategoryProducts);
 router.get("/:id", productController.getProductById);
